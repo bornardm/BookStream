d6ba471a3ae88d28e14a41449c0e6933b77eb8c7
Mael Bornard <mael.bornard@grenoble-inp.org>
2024-05-31 10:54:45 +0200

    gitignore and colors

---
+++ app/constants/colors.js
@@ -0,0 +1,10 @@
+/**
+ * Below are the colors that are used in the app.
+ **/
+
+export const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+  primary: "#6200EE",
+  secondary: "#03DAC6",
+};
