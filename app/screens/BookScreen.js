83bfe871c551643ce4e25a8641dadf564ee76d12
Mael Bornard <mael.bornard@grenoble-inp.org>
2024-06-03 18:10:45 +0200

    bookScreen header images

--- app/screens/BookScreen.js
+++ app/screens/BookScreen.js
@@ -1,23 +1,63 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  ScrollView,
+  Image,
+} from "react-native";
 import { colors } from "../constants/Colors";
 import Icon from "react-native-vector-icons/SimpleLineIcons";
+import { useNavigation } from "@react-navigation/native";
+
+function BackArrow() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.backArrow}>
+      <Icon.Button
+        name="arrow-left"
+        size={20}
+        color={colors.secondary}
+        alignSelf="flex-start"
+        backgroundColor="transparent"
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
+}
+const imageMargin = 20;
 
 export default function BookScreen({ navigation }) {
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 }); // to set the size of the imageView
+
   return (
     <View style={styles.container}>
-      <View style={styles.backArrow}>
-        <Icon.Button
-          name="arrow-left"
-          size={20}
-          color={colors.secondary}
-          alignSelf="flex-start"
-          backgroundColor="transparent"
-          onPress={() => navigation.goBack()}
-        />
-      </View>
-
-      <Text>Here is book screen!</Text>
+      <ScrollView style={styles.ScrollView}>
+        <View
+          style={[
+            styles.imagesView,
+            { height: imageSize.height + 2 * imageMargin },
+          ]}
+        >
+          <Image
+            source={require("../../assets/poter_cover-M.jpg")}
+            blurRadius={3}
+            style={styles.headerImage}
+          />
+          <Image
+            source={require("../../assets/poter_cover-M.jpg")}
+            style={styles.cover}
+            onLayout={(event) => {
+              const { width, height } = event.nativeEvent.layout;
+              setImageSize({ width, height });
+            }}
+          />
+        </View>
+        <Text>Title</Text>
+        <Text>Author</Text>
+      </ScrollView>
+      <BackArrow />
     </View>
   );
 }
@@ -25,15 +65,35 @@ export default function BookScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.primary,
+    backgroundColor: colors.white,
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "row",
   },
   backArrow: {
     backgroundColor: "transparent",
     position: "absolute",
     top: 0,
     left: 0,
+  },
+
+  ScrollView: {
+    width: "100%",
+    backgroundColor: "transparent",
+  },
+  imagesView: {
+    justifyContent: "flex-start",
+    backgroundColor: "transparent",
+  },
+  headerImage: {
+    width: "100%",
+    height: 110,
+  },
+  cover: {
+    width: "45%",
+    borderRadius: 5,
+    margin: imageMargin,
+    alignSelf: "center",
+    position: "absolute",
+    top: 0,
   },
 });
